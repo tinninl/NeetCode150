ebce84d3b0489a31775548b9ec8df10623dc674c
Tinnon Luong <luongtinnon@gmail.com>
2024-11-21 15:26:11 -0500

    b

---
+++ 09_Backtracking/Subsets.py
@@ -0,0 +1,34 @@
+class Solution:
+    def subsets(nums: list[int]) -> list[list[int]]:
+        
+        res = []
+        subset = []
+        
+        def backtrack(i: int):
+            
+            # reach end of array, add subset
+            if i == len(nums):
+                res.append(subset.copy())
+                return
+            
+            for n in nums:
+                subset.append(nums[i])
+                backtrack(i + 1)
+            
+            # include
+            subset.append(nums[i])
+            backtrack(i + 1)
+            #skip
+            subset.pop()
+            backtrack(i + 1)
+            
+        backtrack(0)
+        
+        return res
+            
+            
+            
+        
+        
+        
+        
---
+++ 10_Graphs/Number_Of_Islands.py
@@ -0,0 +1,48 @@
+class Solution:
+    def numIslands(self, grid: list[list[str]]) -> int:
+        
+        # Base case: empty grid
+        if not grid:
+            return 0
+        
+        rows = len(grid)
+        cols = len(grid[0])
+        numIslands = 0
+        
+        # Check every square for an island
+        
+        for r in range(rows):
+            
+            for c in range(cols):
+                
+                if grid[r][c] == "1":
+                    
+                    self.dfs(grid, r, c) # Call dfs to find all connecting land squares
+                    numIslands += 1
+        
+        return numIslands
+    
+    def dfs(self, grid: list[list[int]], r: int, c: c):
+        
+        # If out of bounds, end
+        if (r < 0) or (c < 0) or (r >= len(grid)) or (c >= len(grid[0])):
+            return
+        
+        # If not a land piece, end
+        elif grid[r][c] != "1":
+            return
+        
+        # Turn the land square into a water square (or anything else)
+        grid[r][c] = "0"
+        
+        # Run dfs on all four adjacent squares
+        self.dfs(grid, r + 1, c)
+        self.dfs(grid, r - 1, c)
+        self.dfs(grid, r, c + 1)
+        self.dfs(grid, r, c - 1)
+        
+        
+        
+                    
+        
+        
