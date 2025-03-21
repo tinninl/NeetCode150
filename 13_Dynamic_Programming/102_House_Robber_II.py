e3e7a5cdf44c6884223359558b8affd92a457869
Tinnon Luong <luongtinnon@gmail.com>
2025-03-21 00:45:24 -0400

    Update 102_House_Robber_II.py

--- 13_Dynamic_Programming/102_House_Robber_II.py
+++ 13_Dynamic_Programming/102_House_Robber_II.py
@@ -1,22 +1,23 @@
+"""
+Same as House Robber, except the first and last house are connected.
+The houses form a circle
+"""
 def rob(nums: list[int]):
     
     n = len(nums)
-    
-    cache = [0] * n
-    
-    cache[0] = nums[0]
-    cache[1] = nums[1]
-    
+        
+    if n == 1:  # Check for length of 1 or else index error at dp[1]
+        return nums[0]
+
+    dp = [0] * n
+        
+    dp[0] = nums[0]
+    dp[1] = max(nums[0], nums[1])
+        
     for i in range(2, n):
-        
-        cache[i] = max(cache[i - 1], cache[i - 2] + nums[i])
-        
-    return max(cache[-1] - nums[0], cache[-2])
+        dp[i] = max(dp[i - 1], (dp[i - 2] + nums[i]))
+            
+    return dp[-1]
 
-nums = [2,7,9,3,1]
 
-print(rob(nums))
 
-"""
-since last house is connected to first house, account for that in return stmnt
-"""
